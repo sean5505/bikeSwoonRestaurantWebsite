9d22b827b92e4cc43baec7847e42de66a6c1899e
Steven Gibson <idea40@live.com>
2023-05-02 19:37:32 -0400

    reversion

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    base: '/sean5505/bikeSwoonRestaurantWebsite',
+  
     build: {
       outDir: 'build',
      
