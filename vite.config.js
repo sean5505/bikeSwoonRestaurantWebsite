b3dfd5654d14a1558d2dd872bcaa1117d9272d64
sean5505 <idea40@live.com>
2023-09-26 18:39:54 -0400

    firebase.js removed from .gitignore, data hidden with .env for deployment fix

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig(() => {
   return {
     build: {
-      outDir: 'build',
-     
+      outDir: "build",
     },
     commonjsOptions: {
-      esmExternals: true 
-   },
+      esmExternals: true,
+    },
     plugins: [react()],
     test: {
-        globals: true,
-        environment: 'jsdom',
-        css: true,
-      },
+      globals: true,
+      environment: "jsdom",
+      css: true,
+    },
   };
 });
